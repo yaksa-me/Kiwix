2a7e0dd06a239809aed4245eeb399861ec3b1f9c
automactic <Chrisli@Chriss-MacBook-Pro.local>
2015-01-21 12:22:06 -0500

    URL protocol change

--- Kiwix/KiwixURLProtocol.m
+++ Kiwix/KiwixURLProtocol.m
@@ -33,31 +33,38 @@
     NSURL *requestURL = [self.request URL];
     
     zimReader *reader = [[zimReader alloc] initWithZIMFileURL:[requestURL zimFileURL]];
-    NSString *htmlString = [reader htmlContentOfPageWithPageURL:[requestURL articleURL]];
+    NSString *contentString;
+    
+    [reader exceptionHandling];
     if ([[requestURL pathExtension] caseInsensitiveCompare:@"html"] == NSOrderedSame) {
+        NSString *articleTitle = [[[requestURL contentURLString] componentsSeparatedByString:@"/"] lastObject];
+        articleTitle = [articleTitle stringByReplacingOccurrencesOfString:@".html" withString:@""];
+        contentString = [reader htmlContentOfPageWithPagetitle:articleTitle];
+        /*
         NSRange range = [htmlString rangeOfString:@"<body"];
         
         if(range.location != NSNotFound) {
             // Adjust style for mobile
             float inset = 40;
-            NSString *style = [NSString stringWithFormat:@"<style>div {max-width: %fpx;}</style>", 320 - inset];
+            NSString *style = [NSString stringWithFormat:@"<style>div {max-width: %fpx;}</style>", 400.0];
             htmlString = [NSString stringWithFormat:@"%@%@%@", [htmlString substringToIndex:range.location], style, [htmlString substringFromIndex:range.location]];
             //NSLog(@"%@", htmlString);
-        }
+        }*/
+    } else {
+        contentString = [reader htmlContentOfPageWithPageURLString:[requestURL contentURLString]];
     }
     
-    if ([[requestURL pathExtension] caseInsensitiveCompare:@"css"] == NSOrderedSame) {
-        NSLog(@"%@", htmlString);
-    }
-    NSData *htmlData = [htmlString dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *contentData = [contentString dataUsingEncoding:NSUTF8StringEncoding];
     
-    NSURLResponse *response = [[NSURLResponse alloc] initWithURL:requestURL MIMEType:[requestURL expectedMIMEType] expectedContentLength:[htmlData length] textEncodingName:nil];
+    NSURLResponse *response = [[NSURLResponse alloc] initWithURL:requestURL MIMEType:[requestURL expectedMIMEType] expectedContentLength:[contentData length] textEncodingName:nil];
     
     [self.client URLProtocol:self didReceiveResponse:response cacheStoragePolicy:NSURLCacheStorageAllowed];
-    if (htmlData) {
-        [self.client URLProtocol:self didLoadData:htmlData];
+    if (contentData) {
+        NSLog(@"Load %@ success, length: %ld", [requestURL contentURLString], [contentData length]);
+        [self.client URLProtocol:self didLoadData:contentData];
         [self.client URLProtocolDidFinishLoading:self];
     } else {
+        NSLog(@"Load %@ failed, length: %ld", [requestURL contentURLString], [contentData length]);
         [self.client URLProtocol:self didFailWithError:[NSError errorWithDomain:NSURLErrorDomain code:NSURLErrorFileDoesNotExist userInfo:nil]];
     }
 }
